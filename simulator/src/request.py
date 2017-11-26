8ba8ff58bf764811aa19a2cdc0358aba57bc45aa
shaurabhsingh <33585615+shaurabhsingh@users.noreply.github.com>
2017-11-25 19:59:48 -0500

    Update request.py

--- simulator/src/request.py
+++ simulator/src/request.py
@@ -1,6 +1,7 @@
 
 from transitions.extensions import HierarchicalMachine as Machine
 from transitions.extensions.nesting import NestedState
+from threading import Timer
 
 
 #intializing interface here for testing.
@@ -12,12 +13,23 @@ class interface(object):
 
 class Request(object):
     
-    def __init__(self, interface):
+    def __init__(self, parent, adapter, interface, rel):
 
         class statemachineModel(object):
         
-            def __init__(self,interface = interface):
+            def __init__(self,interface = interface, parent = parent):
                 self.interface = interface
+                self.processing_time_limit = 600.0
+                self.fail_time_limit = 1.0
+                self.failing = False
+                self.parent = parent
+
+            def DEACTIVATE(self):
+                if not self.failing: self.superstate.deactivate()
+                
+            def start_timer(self,timeout):
+                self.timer=Timer(timeout,self.default) #func directs it to the next state???
+                self.timer.start() #no need for a 'kill the timer' method since a new timer request overwrites the timer. should i still do it? Do we need separate timer instances for fail and processing? I dont think so! 
             
             def NOT_READY(self):
                 self.interface.value = "NOT_READY"
@@ -27,18 +39,41 @@ class Request(object):
 
             def ACTIVE(self):
                 self.interface.value = "ACTIVE"
-                
+
             def FAIL(self):
                 self.interface.value = "FAIL"
+                self.start_timer(self.fail_time_limit)
+
+            def COMPLETE_FAILED(self):
+                self.failing = True
+                #self.parent.failed
+                self.failing = False
+
+            def COMPLETE(self):
+                #self.parent.completed
+                pass
+        
+            def PROCESSING(self):
+                self.start_timer(self.processing_time_limit)
+
+            def void(self):
+                pass
 
         self.superstate = statemachineModel(interface)
-        self.interface = self.superstate.interface        
+        self.interface = self.superstate.interface
+        self.related = None
+        if rel: self.related = rel
+        self.parent = self.superstate.parent
+        self.adapter = adapter
+        self.active = True
+        self.failing = self.superstate.failing
 
     def create_statemachine(self):
         NestedState.separator = ':'
         states = [{'name':'base', 'children':['not_ready', 'ready', 'active', 'fail', 'processing']}]
 
-        transitions= [['unavailable','base','base:not_ready'],
+        transitions= [['start','base','base:not_ready'],
+                      ['unavailable','base','base:not_ready'],
                       ['deactivate','base','base:not_ready'],
                       
                       ['activate','base:not_ready','base:active'],
@@ -50,12 +85,19 @@ class Request(object):
                       ['idle','base:active','base:ready'],
                       ['not_ready','base:active','base:ready'],
                       ['failure','base:active','base:fail'],
-                      ['active','base:active','base:processing']]
+                      ['active','base:active','base:processing'],
+                      
+                      ['complete','base:processing','base:not_ready'],
+                      ['default','base:processing','base:fail'],
+                      ['default','base:fail','base:ready'],
+                      ['reset','*','base:not_ready']]
 
         self.statemachine = Machine(model = self.superstate, states = states, transitions = transitions, initial = 'base',ignore_invalid_triggers=True)
-
+        self.superstate.start()
         self.statemachine.on_enter('base:ready', 'READY')
         self.statemachine.on_enter('base:active', 'ACTIVE')
         self.statemachine.on_enter('base:not_ready', 'NOT_READY')
         self.statemachine.on_enter('base:fail', 'FAIL')
+        self.statemachine.on_exit('base:fail', 'COMPLETE_FAILED')
+        self.statemachine.on_enter('base:processing', 'PROCESSING')
         
