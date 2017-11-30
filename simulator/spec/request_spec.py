c47c5be6d06e2792e2e965271e6a1d437bcc0294
shaurabhsingh <33585615+shaurabhsingh@users.noreply.github.com>
2017-11-30 14:16:50 -0500

    Update request_spec.py

--- simulator/spec/request_spec.py
+++ simulator/spec/request_spec.py
@@ -5,7 +5,7 @@ from src.request import *
 
 with description('request'):
     with before.each:
-        self.request = Request(interface())
+        self.request = Request('parent', 'adapter', interface(), True)
         
     with it('should have a request'):
         expect(self.request).to(be_a(Request))
@@ -20,6 +20,7 @@ with description('request'):
         with before.each:
             #Initialize the state machine
             self.request.create_statemachine()
+            self.request.superstate.start()
             #assuming a robot state variable 'bot_interface'. we can account for it later when defining the robot states
             bot_interface="NOT_READY"
             self.bot_interface=bot_interface
@@ -27,47 +28,43 @@ with description('request'):
         with it('should have a statemachine'):
             expect(self.request.statemachine).to(be_a(Machine))
 
-        with it('should expect the intial state to be base'):
-            expect(self.request.superstate.state).to(equal('base'))
-
-        with it('should become not ready when unavailable'):
-            self.request.superstate.unavailable()
+        with it('should expect the intial state to be not_ready'):
             expect(self.request.superstate.state).to(equal('base:not_ready'))
             expect(self.request.interface.value).to(equal('NOT_READY'))
+
+        with it('should remain not ready when unrecognized event'):
+            expect(self.request.superstate.state).to(equal('base:not_ready'))
+            expect(self.request.interface.value).to(equal('NOT_READY'))
+            self.request.superstate.unavailable()
+            expect(self.request.superstate.state).to(equal('base:not_ready'))
+            self.request.superstate.ready()
+            expect(self.request.superstate.state).to(equal('base:not_ready'))
+            self.request.superstate.active()
+            expect(self.request.superstate.state).to(equal('base:not_ready'))     
             
-        with it('should become not ready when deactivate'):
-            self.request.superstate.deactivate()
-            expect(self.request.superstate.state).to(equal('base:not_ready'))
-            expect(self.request.interface.value).to(equal('NOT_READY'))
-
         with it('should become ready when idle'):
-            self.request.superstate.unavailable()
             self.request.superstate.idle()
             expect(self.request.superstate.state).to(equal('base:ready'))
             expect(self.request.interface.value).to(equal('READY'))
 
         with it('should become active when activate'):
-            self.request.superstate.unavailable()
             self.request.superstate.activate()
             expect(self.request.superstate.state).to(equal('base:active'))
             expect(self.request.interface.value).to(equal('ACTIVE'))
 
         with it('should become active from ready when activate'):
-            self.request.superstate.unavailable()
             self.request.superstate.idle()
             self.request.superstate.activate()
             expect(self.request.superstate.state).to(equal('base:active'))
             expect(self.request.interface.value).to(equal('ACTIVE'))
 
         with it('should become ready from active when idle'):
-            self.request.superstate.unavailable()
             self.request.superstate.activate()
             self.request.superstate.idle()
             expect(self.request.superstate.state).to(equal('base:ready'))
             expect(self.request.interface.value).to(equal('READY'))
 
         with it('should become fail from active when failure'):
-            self.request.superstate.unavailable()
             self.request.superstate.activate()
             self.request.superstate.failure()
             expect(self.request.superstate.state).to(equal('base:fail'))
@@ -75,7 +72,6 @@ with description('request'):
 
         with it('should become active from ready when bot is ready'):
             self.bot_interface="READY"
-            self.request.superstate.unavailable()
             self.request.superstate.idle()
             #We could probably add this 'if' statement as a method in request!?
             if self.bot_interface=="READY":
@@ -85,7 +81,6 @@ with description('request'):
 
         with it('should become ready from active when bot is not ready'):
             self.bot_interface="NOT_READY"
-            self.request.superstate.unavailable()
             self.request.superstate.activate()
             #We could probably add this 'if' statement as a method in request!?
             if self.bot_interface=="NOT_READY":
@@ -95,9 +90,52 @@ with description('request'):
 
         with it('should become processing from active when bot is active'):
             self.bot_interface="ACTIVE"
-            self.request.superstate.unavailable()
             self.request.superstate.activate()
             #We could probably add this 'if' statement as a method in request!?
             if self.bot_interface=="ACTIVE":
                 self.request.superstate.active()
                 expect(self.request.superstate.state).to(equal('base:processing'))
+                
+        with it('should become fail from processing when unrecognized event'):
+                self.bot_interface="ACTIVE"
+                self.request.superstate.activate()
+                #We could probably add this 'if' statement as a method in request!?
+                if self.bot_interface=="ACTIVE":
+                    self.request.superstate.active()
+                expect(self.request.superstate.state).to(equal('base:processing'))
+                self.request.superstate.IDLE()
+                time.sleep(0.100)
+                expect(self.request.superstate.state).to(equal('base:fail'))
+                expect(self.request.interface.value).to(equal('FAIL'))
+
+            with it('should become fail from processing when timeout'):
+                self.bot_interface="ACTIVE"
+                self.request.superstate.activate()
+                #We could probably add this 'if' statement as a method in request!?
+                if self.bot_interface=="ACTIVE":
+                    self.request.superstate.active()
+                expect(self.request.superstate.state).to(equal('base:processing'))
+                time.sleep(20.100)
+                expect(self.request.superstate.state).to(equal('base:fail'))
+                expect(self.request.interface.value).to(equal('FAIL'))
+
+            with it('should become ready from fail when unrecognized event'):
+                self.bot_interface="ACTIVE"
+                self.request.superstate.activate()
+                self.request.superstate.failure()
+                expect(self.request.superstate.state).to(equal('base:fail'))
+                expect(self.request.interface.value).to(equal('FAIL'))
+                self.request.superstate.IDLE()
+                time.sleep(0.100)
+                expect(self.request.superstate.state).to(equal('base:ready'))
+                expect(self.request.interface.value).to(equal('READY'))
+
+            with it('should become ready from fail when timeout'):
+                self.bot_interface="ACTIVE"
+                self.request.superstate.activate()
+                self.request.superstate.failure()
+                time.sleep(20.100)
+                expect(self.request.superstate.state).to(equal('base:ready'))
+                expect(self.request.interface.value).to(equal('READY'))
+                
+         
