40ae53dfb260719f06343e047e12fa9fb4cce35d
Shaurabh Singh <ssingh@amtonline.org>
2018-01-10 15:27:41 -0500

    fixed few minor problems

--- simulator/src/request.py
+++ simulator/src/request.py
@@ -63,6 +63,7 @@ class Request(object):
 
             @check_state_calls
             def READY(self):
+                print 'ready here'
                 self.interface.value = "READY"
 
             @check_state_calls
@@ -71,10 +72,15 @@ class Request(object):
 
             @check_state_calls
             def FAILURE(self):
+                time.sleep(0.1)
+                #trnasitioning error without pause!??
                 self.interface.value = "FAIL"
+                
                 check_state_list=[
                     self.FAILURE.has_been_called, self.ACTIVE.has_been_called, self.READY.has_been_called, self.IDLE.has_been_called, self.NOT_READY.has_been_called, self.ACTIVATE.has_been_called, self.COMPLETE.has_been_called, self.DEFAULT.has_been_called
                     ]
+                
+                #print self.FAILURE.has_been_called, self.ACTIVE.has_been_called, self.READY.has_been_called, self.IDLE.has_been_called, self.NOT_READY.has_been_called, self.ACTIVATE.has_been_called, self.COMPLETE.has_been_called, self.DEFAULT.has_been_called
 
                 #all the triggers addressed except active,not_ready,ready which would come from the bot interface. To be done.
                 def complete_check():
@@ -115,13 +121,17 @@ class Request(object):
                             timer_failure.cancel()
                             self.DEFAULT()
                             break
+                    #print self.FAILURE.has_been_called, self.ACTIVE.has_been_called, self.READY.has_been_called, self.IDLE.has_been_called, self.NOT_READY.has_been_called, self.ACTIVATE.has_been_called, self.COMPLETE.has_been_called, self.DEFAULT.has_been_called
                     if self.DEFAULT.has_been_called==check_state_list[7]:
                         self.DEFAULT()
+                    
                         
                 t = Thread(target = complete_check)
                 t.start()
+                
 
             def complete_failed(self):
+                
                 self.failing = True
                 try:
                     self.parent.interface_type(value = 'Request')
@@ -129,6 +139,7 @@ class Request(object):
                 except:
                     "Local Spec Testing"
                 self.failing = False
+                print 'comp fail here'
 
             @check_state_calls 
             def COMPLETE(self):
@@ -147,7 +158,6 @@ class Request(object):
                 check_state_list=[
                     self.FAILURE.has_been_called, self.ACTIVE.has_been_called, self.READY.has_been_called, self.IDLE.has_been_called, self.NOT_READY.has_been_called, self.ACTIVATE.has_been_called, self.DEFAULT.has_been_called
                     ]
-
                 #all the triggers addressed except active,not_ready,ready which would come from the bot interface. To be done.
                 def complete_check():
                     timer_processing = Timer(self.processing_time_limit,self.void)
@@ -191,7 +201,7 @@ class Request(object):
                         
                 t = Thread(target = complete_check)
                 t.start()
-                        
+                
             @check_state_calls
             def RESET(self):
                 self.reset()
