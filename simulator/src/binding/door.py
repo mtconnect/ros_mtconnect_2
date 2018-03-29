03faadc7e4f6f22a61f3d025c09668fc12032267
ssingh@amtonline.org <ssingh@amtonline.org>
2018-03-29 10:26:21 -0400

    Added methods for response/request

--- simulator/src/binding/door.py
+++ simulator/src/binding/door.py
@@ -1,4 +1,5 @@
 from response import *
+from request import *
 
 
 def OpenDoor(parent):
@@ -7,8 +8,20 @@ def OpenDoor(parent):
     OpenDoor.superstate.start()
     return OpenDoor
 
+def OpenDoorRequest(parent):
+    OpenDoor = Request(parent, parent.adapter, parent.open_door, rel = True)
+    OpenDoor.create_statemachine()
+    OpenDoor.superstate.start()
+    return OpenDoor
+
 def CloseDoor(parent):
     CloseDoor = Response(parent, parent.adapter, parent.close_door, 'door', 'CLOSED', 'UNLATCHED',parent.door_state, rel = True, simulate = True)
+    CloseDoor.create_statemachine()
+    CloseDoor.superstate.start()
+    return CloseDoor
+
+def CloseDoorRequest(parent):
+    CloseDoor = Request(parent, parent.adapter, parent.close_door, rel = True)
     CloseDoor.create_statemachine()
     CloseDoor.superstate.start()
     return CloseDoor
