3eedb5d604a7c1054bf701abc8c49ef3b4d85601
Josh Langsfeld <josh.langsfeld@swri.org>
2018-07-20 08:34:41 -0400

    Check for ROS action completion

--- mtconnect_bridge/src/mtconnect_bridge/bridge.py
+++ mtconnect_bridge/src/mtconnect_bridge/bridge.py
@@ -3,9 +3,8 @@ __metaclass__ = type
 
 import rospy
 import actionlib
+from actionlib_msgs.msg import GoalStatus
 from mtconnect_bridge.msg import DeviceWorkAction, DeviceWorkGoal
-
-from simulator.src.binding import robot
 
 ACTIONS = [
     'move',
@@ -13,9 +12,13 @@ ACTIONS = [
     'release',
 ]
 
+TIMEOUT = 20.0
+
 class Bridge:
-    def __init__(self):
-        self.action_client = actionlib.SimpleActionClient("work", DeviceWorkAction)
+    def __init__(self, namespace='ur'):
+        self.action_client = actionlib.SimpleActionClient(namespace + "/work", DeviceWorkAction)
+        if (not self.action_client.wait_for_server(rospy.Duration(TIMEOUT))):
+            rospy.logerr("DeviceWork server not available")
 
     def do_work(self, action_type, work_info):
         rospy.loginfo("Sending '{}' work, data: {}".format(action_type, work_info))
@@ -23,7 +26,16 @@ class Bridge:
         action_goal.type = action_type
         action_goal.data = work_info
         self.action_client.send_goal(action_goal)
-        self.action_client.wait_for_result(rospy.Duration.from_sec(10.0))
+        self.action_client.wait_for_result(rospy.Duration.from_sec(TIMEOUT))
+
+        goal_state = self.action_client.get_state()
+        if goal_state == GoalStatus.PENDING:
+            rospy.logerr("  timed out waiting for work to complete")
+            self.action_client.cancel_goal()
+        elif goal_state == GoalStatus.ABORTED:
+            rospy.logerr("  work was aborted!")
+        elif goal_state == GoalStatus.SUCCEEDED:
+            rospy.loginfo("  work completed successfully")
 
     def spin(self):
         rospy.spin()
