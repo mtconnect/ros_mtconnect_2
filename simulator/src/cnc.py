1c0a4b01ef8658a4475244141be27f3edd9223c1
Shaurabh Kumar Singh <ssingh@amtonline.org>
2018-09-07 17:23:50 -0500

    Update cnc.py

--- simulator/src/cnc.py
+++ simulator/src/cnc.py
@@ -43,7 +43,7 @@ class cnc(object):
 
                 self.system = []
                 
-                self.cycle_time = 10
+                self.cycle_time = 180
 
                 self.load_time_limit(20)
                 self.unload_time_limit(20)
